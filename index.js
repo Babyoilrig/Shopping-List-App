8e3d1b5d9f20a9ade5e52cabd92d413d184d92fe
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2021-12-29 11:21:08 +0000

    Trying to join front and back end - not working yet

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from "express";
 import shoppingList from "./data.js";
+//import path from "path";
 //import bodyParser from "body-parser";
 
 
 const PORT = 3000;
 const app = express();
 
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
-
-//
 
 app.get("/shoppinglist", function (req, res) {
 res.json(shoppingList);
@@ -19,3 +16,21 @@ console.log(shoppingList)
 app.listen(PORT, function(){
     console.log("I'm listening");
 })
+
+export default getShoppingList;
+
+
+// app.post("/", function (req, res) {
+//  const newItem = req.body;
+// console.log("The user gave me:", newItem);
+// createNewItem(newItem);
+// res.json({message: "We did it", success: true, payload: newItem});
+// });
+
+
+
+// function addItems(){
+//     addBoxValue = document.getElementById('additem').value;
+//     shoppingList.push(addBoxValue);
+//     console.log(shoppingList);
+// }
